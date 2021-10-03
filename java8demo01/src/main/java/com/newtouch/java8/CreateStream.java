3a060ebc527e80792d212c6b4ceb7017f64e7940
DESKTOP-CT4OS6H\grzha <grzhangqiang@163.com>
2021-10-03 23:56:17 +0800

    java8

--- java8demo01/src/main/java/com/newtouch/java8/CreateStream.java
+++ java8demo01/src/main/java/com/newtouch/java8/CreateStream.java
@@ -1,20 +1,35 @@
 package com.newtouch.java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * Created by grzha on 2021/10/3.
  */
 public class CreateStream {
 
     public static void main(String[] args) {
-
-
+        fromCollection().forEach(System.out::println);
+        fromValues().forEach(System.out::println);
+        fromArrays().forEach(System.out::println);
 
 
 
 
     }
 
+    public static Stream<String> fromCollection(){
+        List<String> strings = Arrays.asList("hello", "zhangqiang", "world", "java", "lambda");
+        return strings.stream();
+    }
 
+    public static Stream<String> fromValues(){
+        return Stream.of("hello", "zhangqiang", "world", "java", "lambda");
+    }
 
+    public static Stream<String>  fromArrays(){
+        return Arrays.stream(new String[]{"hello", "zhangqiang", "world", "java", "lambda"});
+    }
 
 }
