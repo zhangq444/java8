1573e9808102a432d2e4da84889fd374f0b41bfc
DESKTOP-CT4OS6H\grzha <grzhangqiang@163.com>
2021-10-04 21:39:16 +0800

    java8

--- java8demo01/src/main/java/com/newtouch/java8/StreamReduce.java
+++ java8demo01/src/main/java/com/newtouch/java8/StreamReduce.java
@@ -1,5 +1,9 @@
 package com.newtouch.java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by grzha on 2021/10/4.
  */
@@ -7,13 +11,22 @@ public class StreamReduce {
 
     public static void main(String[] args) {
 
+        List<Integer> integers = Arrays.asList(2, 4, 7, 9, 5, 3, 4, 6, 8);
 
+        Integer total = integers.stream().reduce(0, (identity, integer) -> identity + integer);
+        System.out.println(total);
 
+        integers.stream().reduce(Integer::sum).ifPresent(System.out::println);
+        integers.stream().reduce((i, j) -> i * j).ifPresent(System.out::println);
+        integers.stream().reduce((i,j)->{
+            return i>j?i:j;
+        }).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(Integer::min).get());
+        Optional<Integer> sum = integers.stream().reduce(Integer::sum);
+        sum.ifPresent(System.out::println);
 
 
     }
-
-
 
 
 }
